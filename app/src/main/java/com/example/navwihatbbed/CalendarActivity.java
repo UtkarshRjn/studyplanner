ef1b59bc6caef4ad0bccf7d9abe461619bdbb951
geekyuttu <utkarsh13ranjan@gmail.com>
2021-12-05 17:20:33 +0530

    Adding: Custom Calendar

--- app/src/main/java/com/example/navwihatbbed/CalendarActivity.java
+++ app/src/main/java/com/example/navwihatbbed/CalendarActivity.java
@@ -1,16 +1,20 @@
 package com.example.navwihatbbed;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,23 +29,36 @@ import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
-public class CalendarActivity extends AppCompatActivity {
+public class CalendarActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle toggle;
     Button button;
-    CalendarView calendarView;
+    private RecyclerView calendarRecyclerView;
+    private TextView monthYearText;
+    private LocalDate selectedDate2;
+//    CalendarView calendarView;
     String selectedDate;
     private mySQLiteDBHandler dbHandler;
     TextView num_lecture, num_assignment, num_exam , num_study_plan;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        initWidgets();
+        selectedDate2 = LocalDate.now();
+        setMonthView();
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setTitle("Study Planner");
         Toolbar toolbar = (Toolbar) findViewById(R.id.custom_toolbar);
@@ -58,27 +75,27 @@ public class CalendarActivity extends AppCompatActivity {
         num_exam = findViewById(R.id.num_exam);
         num_study_plan = findViewById(R.id.num_study_plan);
 
-        calendarView = findViewById(R.id.calendarView);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-
-                selectedDate = dayOfMonth + "/" + (month+1) + '/' + year;
-                calendarView.setDateTextAppearance();
-
-                try{
-                    List<Integer> count = dbHandler.getCount(getApplicationContext(),selectedDate);
-//                    Toast.makeText(getApplicationContext(), count.get(0).toString(), Toast.LENGTH_LONG).show();
-                    num_study_plan.setText(count.get(0).toString());
-                    num_assignment.setText(count.get(1).toString());
-                    num_exam.setText(count.get(2).toString());
-                    num_lecture.setText(count.get(3).toString());
-                }catch(Exception e){
-                    Toast.makeText(getApplicationContext(), selectedDate, Toast.LENGTH_LONG).show();
-                }
-
-            }
-        });
+//        calendarView = findViewById(R.id.calendarView);
+//        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+//            @Override
+//            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+//
+//                selectedDate = dayOfMonth + "/" + (month+1) + '/' + year;
+//                calendarView.setDateTextAppearance();
+//
+//                try{
+//                    List<Integer> count = dbHandler.getCount(getApplicationContext(),selectedDate);
+////                    Toast.makeText(getApplicationContext(), count.get(0).toString(), Toast.LENGTH_LONG).show();
+//                    num_study_plan.setText(count.get(0).toString());
+//                    num_assignment.setText(count.get(1).toString());
+//                    num_exam.setText(count.get(2).toString());
+//                    num_lecture.setText(count.get(3).toString());
+//                }catch(Exception e){
+//                    Toast.makeText(getApplicationContext(), selectedDate, Toast.LENGTH_LONG).show();
+//                }
+//
+//            }
+//        });
 
         button = (Button) findViewById(R.id.addevent_button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -108,5 +125,68 @@ public class CalendarActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setMonthView() {
+        monthYearText.setText(monthYearFromDate(selectedDate2));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate2);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private ArrayList<String> daysInMonthArray(LocalDate date) {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+        LocalDate firstOfMonth = selectedDate2.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for(int i=1;i <= 42;i++){
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek){
+                daysInMonthArray.add("");
+            }else{
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return daysInMonthArray;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String monthYearFromDate(LocalDate date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    private void initWidgets(){
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void previousMonthAction(View view){
+        selectedDate2 = selectedDate2.minusMonths(1);
+        setMonthView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void nextMonthAction(View view){
+        selectedDate2 =  selectedDate2.plusMonths(1);
+        setMonthView();
+    }
+
+
+    @Override
+    public void onItemClick(int position, String dayText) {
+        if(dayText.equals("")){
+            String message = "Selected Date" + dayText +  " " + monthYearFromDate(selectedDate2);
+            Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+        }
     }
 }
